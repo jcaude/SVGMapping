644d3c89d99c59f837ac29ebeaf2320bdffb35e5
jcaude <jean-christophe.aude@cea.fr>
2012-03-23 10:20:08 +0100

    [WIP] works on mapping..

--- pkg/SVGMapping/R/Mapping.R
+++ pkg/SVGMapping/R/Mapping.R
@@ -31,6 +31,7 @@ setGenericVerif <- function(name,y){if(!isGeneric(name)){setGeneric(name,y)}else
 MAPPING.Identity <- function(x,p) { return(x) }
 MAPPING.Linear <- function(x,p) { return(p$a*x+p$b) }
 MAPPING.Logistic <- function(x,p) { return(p$K/(1+p$a*exp(-p$r*x))) }
+MAPPING.Sigmoid <- function(x,p) { return(1/(1+exp(-p$r*x))) }
 
 setClass("Mapping",
          representation(data.source="ANY",
@@ -43,10 +44,13 @@ setClass("Mapping",
 
 setGenericVerif(name="dataSource", function(object) { standardGeneric("dataSource") })
 setGenericVerif(name="dataSource<-", function(.Object,value) { standardGeneric("dataSource<-") })
-setGenericVerif(name="mapFunction", function(object) { standardGeneric("mapFunction") })
-setGenericVerif(name="mapFunction<-", function(.Object,value) { standardGeneric("mapFunction<-") })
-setGenericVerif(name="fnParameters", function(object) { standardGeneric("fnParameters") })
-setGenericVerif(name="fnParameters<-", function(.Object,value) { standardGeneric("fnParameters<-") })
+setGenericVerif(name="getFunction", function(object) { standardGeneric("getFunction") })
+setGenericVerif(name="getFunctionParams", function(object) { standardGeneric("getFunctionParams") })
+setGenericVerif(name="fnIdentity", function(.Object) { standardGeneric("fnIdentity") })
+setGenericVerif(name="fnLinear", function(.Object,a,b) { standardGeneric("fnLinear") })
+setGenericVerif(name="fnLogistic", function(.Object,K,a,r) { standardGeneric("fnLogistic") })
+setGenericVerif(name="fnSigmoid", function(.Object,r) { standardGeneric("fnSigmoid") })
+setGenericVerif(name="fnUser", function(.Object,fn,fn.params) { standardGeneric("fnUser") })
 
 
 setMethod(f="initialize", signature="Mapping",
@@ -58,8 +62,8 @@ setMethod(f="initialize", signature="Mapping",
             ## init.
             .Object@svg <- value
             .Object@data.source <- NULL
-            .Object@fn <- NULL
-            .Object@fn.parameters <- NULL
+            .Object@fn <- MAPPING.Identity
+            .Object@fn.parameters <- list()
             .Object@animation <- FALSE
 
             ## eop
@@ -84,39 +88,115 @@ setReplaceMethod(f="dataSource", signature="Mapping",
                  }
                  )
 
-setMethod(f="mapFunction", signature="Mapping",
+setMethod(f="getFunction", signature="Mapping",
           definition=function(object)
           {
             return(object@fn)
           }
           )
           
-setReplaceMethod(f="mapFunction", signature="Mapping",
-                 definition=function(.Object,value)
-                 {
-                   ## !!!!! CHECKING ??
-                   ## eop
-                   .Object@fn <- value
-                   return(.Object)
-                 }
-                 )
-
-setMethod(f="fnParameters", signature="Mapping",
+setMethod(f="getFunctionParams", signature="Mapping",
           definition=function(object)
           {
             return(object@fn.parameters)
           }
           )
+
+setMethod(f="fnIdentity", signature="Mapping",
+          definition=function(.Object)
+          {
+            ## init.
+            namedOjbect <- deparse(substitute(.Object))
+
+            ## update
+            .Object@fn <- MAPPING.Identity
+            .Object@fn.parameters <- list()
+            
+            ## eop
+            assign(namedOjbect, .Object, envir=parent.frame())
+          }
+          )
           
-setReplaceMethod(f="fnParameters", signature="Mapping",
-                 definition=function(.Object,value)
-                 {
-                   ## check
-                   if(!is.list(value))
-                     stop("'value' must be a list of parameters")
-                   
-                   ## eop
-                   .Object@fn.parameters <- value
-                   return(.Object)
-                 }
-                 )
+setMethod(f="fnLinear", signature="Mapping",
+          definition=function(.Object,a,b)
+          {
+            ## init.
+            namedOjbect <- deparse(substitute(.Object))
+            a <- if(missing(a)) 1 else a
+            b <- if(missing(b)) 0 else b
+
+            ## check
+            if(!is.numeric(a) || !is.numeric(b))
+              stop("linear (a*x+b) parameters 'a' and 'b' must be numeric") 
+
+            ## update
+            .Object@fn <- MAPPING.Linear
+            .Object@fn.parameters <- list(a=a,b=b)
+            
+            ## eop
+            assign(namedOjbect, .Object, envir=parent.frame())
+          }
+          )
+
+setMethod(f="fnLogistic", signature="Mapping",
+          definition=function(.Object,K,a,r)
+          {
+            ## init.
+            namedOjbect <- deparse(substitute(.Object))
+            K <- if(missing(K)) 1 else K
+            a <- if(missing(a)) 1 else a
+            r <- if(missing(r)) 1 else r
+
+            ## check
+            if(!is.numeric(K) || !is.numeric(a) || !is.numeric(r))
+              stop("logistic (K/(1+a*exp(-rx)) parameters 'K','a' and 'r' must be numeric") 
+
+            ## update
+            .Object@fn <- MAPPING.Logistic
+            .Object@fn.parameters <- list(K=K,a=a,r=r)
+            
+            ## eop
+            assign(namedOjbect, .Object, envir=parent.frame())
+          }
+          )
+
+setMethod(f="fnSigmoid", signature="Mapping",
+          definition=function(.Object,r)
+          {
+            ## init.
+            namedOjbect <- deparse(substitute(.Object))
+            r <- if(missing(r)) 1 else r
+
+            ## check
+            if(!is.numeric(r))
+              stop("Sigmoid (1/(1+exp(-rx)) parameter 'r' must be numeric") 
+
+            ## update
+            .Object@fn <- MAPPING.Sigmoid
+            .Object@fn.parameters <- list(r=r)
+            
+            ## eop
+            assign(namedOjbect, .Object, envir=parent.frame())
+          }
+          )
+
+setMethod(f="fnUser", signature="Mapping",
+          definition=function(.Object,fn,fn.params)
+          {
+            ## check
+            if(missing(fn) || !is.function(fn))
+              stop("You must provide a valid function 'fn'")           
+            if(missing(fn.params) || !is.list(fn.params))
+              stop("You must provide a valid list of parameters") 
+            
+            ## init.
+            namedOjbect <- deparse(substitute(.Object))          
+
+            ## update
+            .Object@fn <- fn
+            .Object@fn.parameters <- fn.params
+            
+            ## eop
+            assign(namedOjbect, .Object, envir=parent.frame())
+          }
+          )
