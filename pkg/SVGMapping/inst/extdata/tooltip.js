5a4f5de015eb647a0d08f9ab31db260c1e3e101e
jcaude <jean-christophe.aude@cea.fr>
2012-04-05 18:09:47 +0200

    [UPD] code a standard tooltip

--- pkg/SVGMapping/inst/extdata/tooltip.js
+++ pkg/SVGMapping/inst/extdata/tooltip.js
@@ -3,117 +3,150 @@
 
 var annotElements = new Array()
 
+function legacyTooltip(evt,text) {
+
+    // locals
+    var h_max = 240;
+    var w_max = 400;
+    var w_margin = 20;
+    var h_margin = 10;
+
+    // build a text item and adjust to BBox
+    svg_text = svgDocument.createElementNS(svgNS, "text")
+    svg_text.appendChild(svgDocument.createTextNode(text))
+    svg_text.setAttributeNS(null,"font-size", "80%")
+    svgDocument.rootElement.appendChild(svg_text)
+    var text_w = svg_text.getComputedTextLength()
+    var text_len = text.length
+    while(text_w + w_margin > w_max && text_w > 10) {
+	text_len = text_len - 1
+	svg_text.firstChild.replaceWholeText(text.substr(0, l) + "...")
+	text_w = svg_text.getComputedTextLength()
+    }
+    var text_h = svg_text.getComputedTextHeight()
+    var font_size = 80
+    while(text_h + h_margin > h_max && font_size > 20) {
+	font_size = font_size - 1
+	svg_text.setAttributeNS(null,"font-size", font_size + "%")
+	text_h = svg_text.getComputedTextHeight()
+    }
+    
+    
+}
+
 function displayAnnotation(evt, name, description, foldchanges, colors)
 {
-	var n = foldchanges.length
+    var n = foldchanges.length
 
-	var h = 110;
-	var w = 400;
+    var h = 110;
+    var w = 400;
 
-	ww = svgDocument.rootElement.getBBox().width;
-	wh = svgDocument.rootElement.getBBox().height;
+    ww = svgDocument.rootElement.getBBox().width;
+    wh = svgDocument.rootElement.getBBox().height;
 
-	bgrect = svgDocument.createElementNS(svgNS, "rect")
-	bgrect.setAttributeNS(null, "width", w)
-	bgrect.setAttributeNS(null, "height", h)
-	bgrect.setAttributeNS(null, "fill", "#eeeeee")
-	bgrect.setAttributeNS(null, "stroke", "black")
-	svgDocument.rootElement.appendChild(bgrect)
-	annotElements.push(bgrect);
+    bgrect = svgDocument.createElementNS(svgNS, "rect")
+    bgrect.setAttributeNS(null, "width", w)
+    bgrect.setAttributeNS(null, "height", h)
+    bgrect.setAttributeNS(null, "fill", "#eeeeee")
+    bgrect.setAttributeNS(null, "stroke", "black")
+    svgDocument.rootElement.appendChild(bgrect)
+    annotElements.push(bgrect);
 
-	if (description != null) {
-		descel = svgDocument.createElementNS(svgNS, "text")
-		descel.appendChild(svgDocument.createTextNode(description))
-		descel.setAttributeNS(null, "font-size", "80%")
-		svgDocument.rootElement.appendChild(descel)
-		var tw = descel.getComputedTextLength()
-		var l = description.length
-		while (tw + 20 > w && tw > 10) {
-			l = l - 1
-			descel.firstChild.replaceWholeText(description.substr(0, l) + "...")
-			tw = descel.getComputedTextLength()
-		}
-		annotElements.push(descel);
+    if (description != null) {
+	descel = svgDocument.createElementNS(svgNS, "text")
+	descel.appendChild(svgDocument.createTextNode(description))
+	descel.setAttributeNS(null, "font-size", "80%")
+	svgDocument.rootElement.appendChild(descel)
+	var tw = descel.getComputedTextLength()
+	var l = description.length
+	while (tw + 20 > w && tw > 10) {
+	    l = l - 1
+	    descel.firstChild.replaceWholeText(description.substr(0, l) + "...")
+	    tw = descel.getComputedTextLength()
 	}
+	annotElements.push(descel);
+    }
 
-	bgrectTitle = svgDocument.createElementNS(svgNS, "rect")
-	bgrectTitle.setAttributeNS(null, "width", w)
-	bgrectTitle.setAttributeNS(null, "height", 30)
-	bgrectTitle.setAttributeNS(null, "fill", "#dddddd")
-	bgrectTitle.setAttributeNS(null, "stroke", "black")
-	svgDocument.rootElement.appendChild(bgrectTitle)
-	annotElements.push(bgrectTitle);
+    bgrectTitle = svgDocument.createElementNS(svgNS, "rect")
+    bgrectTitle.setAttributeNS(null, "width", w)
+    bgrectTitle.setAttributeNS(null, "height", 30)
+    bgrectTitle.setAttributeNS(null, "fill", "#dddddd")
+    bgrectTitle.setAttributeNS(null, "stroke", "black")
+    svgDocument.rootElement.appendChild(bgrectTitle)
+    annotElements.push(bgrectTitle);
 
-	title1el = svgDocument.createElementNS(svgNS, "text")
-	title1el.setAttributeNS(null, "font-weight", "bold")
-	title1el.appendChild(svgDocument.createTextNode(name))
-	svgDocument.rootElement.appendChild(title1el)
-	annotElements.push(title1el);
+    title1el = svgDocument.createElementNS(svgNS, "text")
+    title1el.setAttributeNS(null, "font-weight", "bold")
+    title1el.appendChild(svgDocument.createTextNode(name))
+    svgDocument.rootElement.appendChild(title1el)
+    annotElements.push(title1el);
 
-	var cw = (w-20)/n
-	var boxes = new Array(n)
-	var fcs = new Array(n)
-	for (var i=0; i<n; i++) {
-		el = svgDocument.createElementNS(svgNS, "rect")
-		el.setAttributeNS(null, "width", cw)
-		el.setAttributeNS(null, "height", 30)
-		el.setAttributeNS(null, "fill", colors[i])
-		el.setAttributeNS(null, "stroke", "black")
-		svgDocument.rootElement.appendChild(el)
-		annotElements.push(el);
-		boxes[i] = el
-		elt = svgDocument.createElementNS(svgNS, "text")
-		elt.appendChild(svgDocument.createTextNode(foldchanges[i]))
-		elt.setAttributeNS(null, "font-family", "Arial")
-		fcval = parseFloat(foldchanges[i])
-		elt.setAttributeNS(null, "fill", "black")
-		if (colors[i].charAt(0) == '#' && colors[i].length == 7) {
-			var c = colors[i].toLowerCase();
-			if (parseInt(c.substring(1,3),16)*299 + parseInt(c.substring(3,5),16)*587 + parseInt(c.substring(5,7),16)*114 < 125000) {
-				// This background color appears quite dark, let's write the text in white
-				elt.setAttributeNS(null, "fill", "white")
-			}
-		}
-		svgDocument.rootElement.appendChild(elt)
-		annotElements.push(elt);
-		fcs[i] = elt
+    var cw = (w-20)/n
+    var boxes = new Array(n)
+    var fcs = new Array(n)
+    for (var i=0; i<n; i++) {
+	el = svgDocument.createElementNS(svgNS, "rect")
+	el.setAttributeNS(null, "width", cw)
+	el.setAttributeNS(null, "height", 30)
+	el.setAttributeNS(null, "fill", colors[i])
+	el.setAttributeNS(null, "stroke", "black")
+	svgDocument.rootElement.appendChild(el)
+	annotElements.push(el);
+	boxes[i] = el
+	elt = svgDocument.createElementNS(svgNS, "text")
+	elt.appendChild(svgDocument.createTextNode(foldchanges[i]))
+	elt.setAttributeNS(null, "font-family", "Arial")
+	fcval = parseFloat(foldchanges[i])
+	elt.setAttributeNS(null, "fill", "black")
+	if (colors[i].charAt(0) == '#' && colors[i].length == 7) {
+	    var c = colors[i].toLowerCase();
+	    if (parseInt(c.substring(1,3),16)*299 + 
+		parseInt(c.substring(3,5),16)*587 + 
+		parseInt(c.substring(5,7),16)*114 < 125000) {
+		// This background color appears quite dark, let's write the text in white
+		elt.setAttributeNS(null, "fill", "white")
+	    }
 	}
+	svgDocument.rootElement.appendChild(elt)
+	annotElements.push(elt);
+	fcs[i] = elt
+    }
 
-	// Compute box position
-	x = evt.clientX + 9 + window.pageXOffset;
-	y = evt.clientY + 17 + window.pageYOffset;
-	if (x + w > ww) {
-		// The tooltip is out of the SVG document (on the right), so display it on the left of the mouse cursor
-		x = x - w;
-	}
-	if (y + h > wh) {
-		// Same idea, when tooltip is below the document bottom
-		y = y - h - 25;
-	}
+    // Compute box position
+    x = evt.clientX + 9 + window.pageXOffset;
+    y = evt.clientY + 17 + window.pageYOffset;
+    if (x + w > ww) {
+	// The tooltip is out of the SVG document (on the right), so display it on the left of the mouse cursor
+	x = x - w;
+    }
+    if (y + h > wh) {
+	// Same idea, when tooltip is below the document bottom
+	y = y - h - 25;
+    }
 
-	// Put all elements on the right place
-	bgrect.setAttributeNS(null, "x", x)
-	bgrect.setAttributeNS(null, "y", y)
-	bgrectTitle.setAttributeNS(null, "x", x)
-	bgrectTitle.setAttributeNS(null, "y", y)
-	title1el.setAttributeNS(null, "x", x + 10)
-	title1el.setAttributeNS(null, "y", y + 20)
-	if (description != null) {
-		descel.setAttributeNS(null, "x", x + 10)
-		descel.setAttributeNS(null, "y", y + 50)
-	}
-	for (var i=0; i<n; i++) {
-		boxes[i].setAttributeNS(null, "y", y + 70)
-		boxes[i].setAttributeNS(null, "x", x + 10 + cw*i)
-		fcs[i].setAttributeNS(null, "y", y + 90)
-		fcs[i].setAttributeNS(null, "x", x + 10 + cw*i + cw/2 - fcs[i].getComputedTextLength()/2)
-	}
+    // Put all elements on the right place
+    bgrect.setAttributeNS(null, "x", x)
+    bgrect.setAttributeNS(null, "y", y)
+    bgrectTitle.setAttributeNS(null, "x", x)
+    bgrectTitle.setAttributeNS(null, "y", y)
+    title1el.setAttributeNS(null, "x", x + 10)
+    title1el.setAttributeNS(null, "y", y + 20)
+    if (description != null) {
+	descel.setAttributeNS(null, "x", x + 10)
+	descel.setAttributeNS(null, "y", y + 50)
+    }
+    for (var i=0; i<n; i++) {
+	boxes[i].setAttributeNS(null, "y", y + 70)
+	boxes[i].setAttributeNS(null, "x", x + 10 + cw*i)
+	fcs[i].setAttributeNS(null, "y", y + 90)
+	fcs[i].setAttributeNS(null, "x", x + 10 + cw*i + cw/2 - fcs[i].getComputedTextLength()/2)
+    }
 }
 
 function hideAnnotation(evt)
 {
-	while (annotElements.length > 0) {
-		el = annotElements.pop();
-		el.parentNode.removeChild(el);
-	}
+    while (annotElements.length > 0) {
+	el = annotElements.pop();
+	el.parentNode.removeChild(el);
+    }
 }
