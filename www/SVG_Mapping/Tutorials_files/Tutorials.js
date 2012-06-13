012f4fde74b886d0b840163aab804f1d50505868
Jean-Christophe AUDE <jc.aude@gmail.com>
2012-06-13 16:00:16 -0500

    [UPD] remove the no longer necessary Rforge website code

--- www/SVG_Mapping/Tutorials_files/Tutorials.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 3.0.4 local-build-20111007
-
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('Tutorials_files/TutorialsMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();performPostEffectsFixups()}
-function onPageUnload()
-{Widget.onunload();}
