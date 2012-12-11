df3db7893dbb7bf2ed3fd4f8fe553018d99aa80a
Jean-Christophe AUDE <jean-christophe.aude@cea.fr>
2012-12-11 17:23:32 +0100

    [WIP] Start converting code from SVG.R@V2.. expected heavy refactoring here (already started!). Note: original file will be split into chunks (easier to maintain)

---
+++ SVGMapping-devtools/R/SVG-defs.R
@@ -0,0 +1,155 @@
+## Copyright (c) 2011, CEA DSV/iBiTecS
+## All rights reserved.
+## 
+## Redistribution and use in source and binary forms, with or without modification,
+## are permitted provided that the following conditions are met:
+## 
+## * Redistributions of source code must retain the above copyright notice, this list
+##   of conditions and the following disclaimer.
+## 
+## * Redistributions in binary form must reproduce the above copyright notice, this
+##   list of conditions and the following disclaimer in the documentation and/or
+##   other materials provided with the distribution.
+## 
+## THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+## ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+## WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+## DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+## ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+## (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+## LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+## ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+## (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+## SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+## S V G  -- DEFINITIONS
+## --------------------------------------------------
+
+## - internal stuff..
+svgNS <- "http://www.w3.org/2000/svg"
+SVG.VALUE <- factor("@VALUE@")
+
+.completeNamespaces <- function(svgdata) {
+  NS <- xmlNamespaceDefinitions(svgdata, simplify=TRUE)
+  NS[["svg"]] <- svgNS
+  return(NS)
+}
+
+#' SVG class
+#' 
+#' SVG is the main classs of SVGMapping. It is used to load, save and render 
+#' SVG files. Futhermore, all mappings, layout or pseudo-devices operations are
+#' applied on SVG objects. 
+#' 
+#' @name SVG
+#' @exportClass "SVG"
+#' @aliases SVG-class
+setClass("SVG",
+         representation(svg="ANY",
+                        default_search_attr="character",
+                        js.animation="logical",
+                        js.inlines="list",
+                        js.hrefs="list")
+)
+
+
+setMethod(f="initialize", signature="SVG",
+          definition=function(.Object,...)
+          {
+            ## - locals
+            .arg <- function(name,default.value) {
+              if(sum(grepl(paste("^",name,"$",sep=""), args.names)) > 0) {
+                v <- args[[grep(paste("^",name,"$",sep=""),args.names)]]
+                return(v)
+              }
+              else {
+                return(default.value)
+              }
+            }
+            
+            ## get args
+            args <- list(...)
+            args.names <- names(args)
+            if(is.null(args.names)) args.names <- list()
+            
+            ## init. @TODO: To be refactored
+#             .Object@svg <- xmlTreeParse(system.file("extdata/blank.svg", package="SVGMapping"),
+#                                         useInternalNodes=TRUE,
+#                                         addAttributeNamespaces=TRUE,
+#                                         fullNamespaceInfo=FALSE)
+            
+            ## init.
+            defaultSearchAttr(.Object) <- "id"
+            jsAnimations(.Object) <- FALSE
+            .Object@js.hrefs <- list()
+            .Object@js.inlines <- list()
+            
+            ## eop
+            return(.Object)
+          }
+)
+
+## STANDARD DOCUMENT DIMENSIONS
+## ----------------------------
+
+.SVG.STD.DIMS <-
+  list("us.letter" = c("8.5in","11in"),
+       "us.legal" = c("8.5in","14in"),
+       "us.executive" = c("7.2in","10.5in"),
+       "a0" = c("841mm","1189mm"),
+       "a1" = c("594mm","841mm"),
+       "a2" = c("420mm","594mm"),
+       "a3" = c("297mm","420mm"),
+       "a4" = c("210mm","297mm"),
+       "a5" = c("148mm","210mm"),
+       "a6" = c("105mm","148mm"),
+       "a7" = c("74mm","105mm"),
+       "a8" = c("52mm","74mm"),
+       "a9" = c("37mm","52mm"),
+       "a10" = c("26mm","37mm"),
+       "b0" = c("1000mm","1414mm"),
+       "b1" = c("707mm","1000mm"),
+       "b2" = c("500mm","707mm"),
+       "b3" = c("353mm","500mm"),
+       "b4" = c("250mm","353mm"),
+       "b5" = c("176mm","250mm"),
+       "b6" = c("125mm","176mm"),
+       "b7" = c("88mm","125mm"),
+       "b8" = c("62mm","88mm"),
+       "b9" = c("44mm","62mm"),
+       "b10" = c("31mm","44mm"),
+       "c0" = c("917mm","1297mm"),
+       "c1" = c("648mm","917mm"),
+       "c2" = c("458mm","648mm"),
+       "c3" = c("324mm","458mm"),
+       "c4" = c("229mm","324mm"),
+       "c5" = c("162mm","229mm"),
+       "c6" = c("114mm","162mm"),
+       "c7" = c("81mm","114mm"),
+       "c8" = c("57mm","81mm"),
+       "c9" = c("40mm","57mm"),
+       "c10" = c("28mm","40mm"),
+       "d1" = c("545mm","771mm"),
+       "d2" = c("385mm","545mm"),
+       "d3" = c("272mm","385mm"),
+       "d4" = c("192mm","272mm"),
+       "d5" = c("136mm","192mm"),
+       "d6" = c("96mm","136mm"),
+       "d7" = c("68mm","96mm"),
+       "e3" = c("400mm","560mm"),
+       "e4" = c("280mm","400mm"),
+       "e5" = c("200mm","280mm"),
+       "e6" = c("140mm","200mm"),
+       "us.#10.envelope" = c("41.in","9.5in"),
+       "dl.envelop" = c("110mm","220mm"),
+       "ledger.tabloid" = c("11in","17in"),
+       "card.iso7810" = c("54mm","85.6mm"),
+       "card.us" = c("2in","3.5in"),
+       "card.eu" = c("55mm","85mm"),
+       "arch.a" = c("9in","12in"),
+       "arch.b" = c("12in","18in"),
+       "arch.c" = c("18in","24in"),
+       "arch.d" = c("24in","36in"),
+       "arch.e" = c("36in","48in"),
+       "arch.e1" = c("30in","42in")
+  )
---
+++ SVGMapping-devtools/R/SVG-javascript.R
@@ -0,0 +1,287 @@
+## Copyright (c) 2011, CEA DSV/iBiTecS
+## All rights reserved.
+## 
+## Redistribution and use in source and binary forms, with or without modification,
+## are permitted provided that the following conditions are met:
+## 
+## * Redistributions of source code must retain the above copyright notice, this list
+##   of conditions and the following disclaimer.
+## 
+## * Redistributions in binary form must reproduce the above copyright notice, this
+##   list of conditions and the following disclaimer in the documentation and/or
+##   other materials provided with the distribution.
+## 
+## THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+## ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+## WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+## DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+## ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+## (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+## LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+## ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+## (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+## SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+## S V G  -- JAVASCRIPTS
+## --------------------------------------------------
+
+#' 
+#' SVG Javascript Accessors
+#' 
+#' These methods allows to associate some javascript code to an SVG document. 
+#' This code could be directly attache to the document or referenced as an 
+#' external document.
+#' 
+#' The \code{jsAnimation(object)} method returns \code{TRUE} if the SVG document
+#' have some animations or \code{FALSE} otherwise.
+#' 
+#' @name jsAnimation
+#'   
+#' @param object is the SVG document object
+#'   
+#' @return \code{jsAnimations} returns a logical value
+#'   
+#' @rdname svg.js-methods
+#' @exportMethod jsAnimation
+#' @docType methods
+setGeneric(name="jsAnimation", function(object) {standardGeneric("jsAnimation")})
+
+#' <title already defined>
+#' 
+#' 
+#' 
+#' The \code{jsAnimation(object) <- value} method allows to enable or disable
+#' the SVGMapping animation engine. Here, \emph{value} is logical.
+#' 
+#' @name jsAnimation<-
+#' 
+#' @rdname svg.js-methods
+#' @exportMethod jsAnimation<-
+#' @docType methods
+setGeneric(name="jsAnimation<-", function(.Object,value) {standardGeneric("jsAnimation<-")})
+
+#' <title already defined>
+#' 
+#' 
+#' 
+#' The \code{jsHRef(object)} method returns the list of Javascript HREF that 
+#' are associated with the SVG documemnt.
+#' 
+#' @name jsHRef
+#' 
+#' @return \code{jsHRef} and \code{jsInlines} return lists
+#'   
+#' @rdname svg.js-methods
+#' @exportMethod jsHRef
+#' @docType methods
+setGeneric(name="jsHRef", function(object) {standardGeneric("jsHRef")})
+
+#' <title already defined>
+#' 
+#' 
+#' 
+#' The \code{jsHRef(object) <- value} method associates a named list of 
+#' Javascript HREF with the SVG documemnt. List names are used as identifiers
+#' and must be unique across the whole SVG document. One can use the \code{uid}
+#' method to automatically forge new IDs.
+#' 
+#' @name jsHRef<-
+#'   
+#' @rdname svg.js-methods
+#' @exportMethod jsHRef<-
+#' @docType methods
+NULL
+
+#' <title already defined>
+#' 
+#' 
+#' 
+#' The \code{jsInlines(object)} method returns the list of inline Javascript
+#' scripts that are associated with the SVG documemnt.
+#' 
+#' @name jsInlines
+#'   
+#' @rdname svg.js-methods
+#' @exportMethod jsInlines
+#' @docType methods
+setGeneric(name="jsInlines", function(object) {standardGeneric("jsInlines")})
+
+#' <title already defined>
+#' 
+#' 
+#' 
+#' The \code{jsInlines(object) <- value} method associates a named list of
+#' inline Javascripts with the SVG documemnt.
+#' 
+#' @name jsInlines<-
+#'   
+#' @rdname svg.js-methods
+#' @exportMethod jsInlines<-
+#' @docType methods
+NULL
+
+#' <title already defined>
+#' 
+#' 
+#' 
+#' The \code{addScript(object)} method 
+setGenericVerif(name="addScript", function(object,script,id) {standardGeneric("addScript")})
+
+#' @rdname svg.js-methods
+#' @aliases jsAnimation,SVG-method
+setMethod(f="jsAnimation", signature="SVG",
+          definition=function(object)
+          {
+            return(object@js.animation)
+          }
+)
+
+#' @name jsAnimation<-,SVG-method
+#' @rdname svg.js-methods
+#' @aliases jsAnimation<-,SVG-method
+setReplaceMethod(f="jsAnimation", signature="SVG",
+                 definition=function(.Object, value)
+                 {
+                   ## check
+                   if(!is.logical(value))
+                     stop("'value' must be logical")
+                   
+                   ## eop
+                   .Object@js.animation <- value
+                   return(.Object)
+                 }
+)
+
+#' @rdname svg.js-methods
+#' @aliases jsHRef,SVG-method
+setMethod(f="jsHRef", signature="SVG",
+          definition=function(object)
+          {
+            return(object@js.hrefs)
+          }
+)
+
+#' @name jsHRef<-,SVG-method
+#' @rdname svg.js-methods
+#' @aliases jsHRef<-,SVG-method
+setReplaceMethod(f="jsHRef", signature="SVG",
+                 definition=function(.Object,value)
+                 {
+                   # check
+                   if(!is.list(value))
+                     stop("'value' must be a list")
+                   if(is.null(names(value)))
+                     stop("'value' must be a named (ID) list")
+                   if(length(value)==0)
+                     return(.Object)                   
+                   
+                   # init.
+                   l.names <- names(value)
+                   l.names <- l.names[l.names != ""]
+                   if(length(l.names) != length(value))
+                     stop("all 'value' items must have a (non empty) ID")
+                   
+                   ## update js file list
+                   for(i in 1:length(l.names)) {
+                     name <- l.names[[i]]                     
+                     .Object@js.hrefs[name] <- value[[i]]
+                   }
+                   
+                   ## eop                
+                   return(.Object)
+                 }
+)
+
+#' @rdname svg.js-methods
+#' @aliases jsInlines,SVG-method
+setMethod(f="jsInlines", signature="SVG",
+          definition=function(object)
+          {
+            return(object@js.inlines)
+          }
+)
+
+#' @name jsInlines<-,SVG-method
+#' @rdname svg.js-methods
+#' @aliases jsInlines<-,SVG-method
+setReplaceMethod(f="jsInlines", signature="SVG",
+                 definition=function(.Object,value)
+                 {
+                   # check
+                   if(!is.list(value))
+                     stop("'value' must be a list")
+                   if(is.null(names(value)))
+                     stop("'value' must be a ID list")
+                    if(length(value)==0)
+                      return(.Object)
+                   
+                   # init.
+                   l.names <- names(value)
+                   l.names <- l.names[l.names != ""]
+                   if(length(l.names) != length(value))
+                     stop("all 'value' items must have a (non empty) ID")
+                   
+                   ## update js file list
+                   for(i in 1:length(l.names)) {
+                     name <- l.names[[i]]                     
+                     .Object@js.inlines[name] <- value[[i]]
+                   }
+                   
+                   ## eop                
+                   return(.Object)
+                 }
+)
+
+setMethod(f=".xml_scripts", signature="SVG",
+          definition=function(object,id)
+          {
+            
+            ## -- process href scripts
+            .proc_js_href <- function(object, id) {
+              
+              # check
+              if( !(id %in% names(jsHRef(object))) )
+                stop("Can't find script among inlines/files script definitions")
+              
+              # init
+              js_attrs <- list(id=id, 'xlink:href'=jsHRef(object)[[id]])
+              js_node <- newXMLNode("script", attrs=js_attrs)
+              
+              # eop
+              return(js_node)
+            }
+            
+            ## -- process inline scripts
+            .proc_js_inline <- function(object, id) {
+
+              # check
+              if( !(id %in% names(jsInlines(object))) )
+                stop("Can't find script among inlines/files script definitions")
+              
+              # init.
+              script <- jsInlines(object)[[id]]
+              
+              # build script nodes
+              cdata <- newXMLCDataNode(paste("\n", script, "\n", sep=""))
+              js_attrs <- list(type="text/ecmascript", id=id)
+              js_node <- newXMLNode("script", attrs=js_attrs, .children=list(cdata))
+              
+              # eop
+              return(js_node)
+              
+            }
+            
+            ## HERE -------
+            ## ------------ Missing ID .. process all scripts href/inlines and             
+            
+            ## Update/Insert script
+            updated <- FALSE
+            search.node <- object[paste("id::",id,sep="")]
+            if(length(search.node) > 0) {
+              replaceNodes(search.node[[1]], scriptnode)
+              updated <- TRUE
+            }
+            if(!updated)
+              addChildren(xmlRoot(object@svg), kids=list(scriptnode))            
+          }
+)
